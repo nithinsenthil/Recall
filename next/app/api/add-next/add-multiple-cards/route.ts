f271d697fa9d898c122e835800c97e5ea9ca545b
Nithin Senthil <nithin020304@gmail.com>
2024-04-28 10:37:48 -0700

    Fixed Add-Multiple-Cards
    Fixed issue where endpoint would never finish.

--- next/app/api/add-next/add-multiple-cards/route.ts
+++ next/app/api/add-next/add-multiple-cards/route.ts
@@ -17,29 +17,35 @@ export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
     let body_nlp
-    num_cards: Number = body["num_cards"]
+    let num_cards = body["num_cards"]
+    let cards = body["cards"]
 
-    for (var curr_card of body["cards"]) {
-      let curr_body = await curr_card.json();
+    for (var i = 0; i < num_cards; i++) {
 
-      const res_nlp = await fetch("http://localhost:8000/embed-card", {body: JSON.stringify({definition: curr_body["definition"]}), method: "POST"})
+      const res_nlp = await fetch("http://localhost:8000/embed-card", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({definition: cards[i]["definition"]}),
+        method: "POST"})
       body_nlp = await res_nlp.json()
-      
+
       const {data: data1, error: error1} = await supabase
       .from ("cards")
       .insert({
-      deck_id: curr_body["deck_id"], 
-      term: curr_body["term"],
-      definition: curr_body["definition"],
-      ease_factor: curr_body["ease_factor"],
-      graduated: curr_body["graduated"],
-      interval: curr_body["interval"],
-      next_review: curr_body["next_review"],
+      deck_id: cards[i]["deck_id"], 
+      term: cards[i]["term"],
+      definition: cards[i]["definition"],
+      ease_factor: cards[i]["ease_factor"],
+      graduated: cards[i]["graduated"],
+      interval: cards[i]["interval"],
+      next_review: cards[i]["next_review"],
       named_entities: body_nlp["named_entities"],
       embeddings: body_nlp["embeddings"],
       dependencies: body_nlp["dependencies"]})
       .select()
       
+    
       if (data1 == null) {
           return NextResponse.json({error: true})
       }
